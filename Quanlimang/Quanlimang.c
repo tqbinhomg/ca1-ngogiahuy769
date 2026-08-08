2adf6ddc57c3f6766cdb69b0aed853d9be9f6baa
unknown <ngogiahuy769@gmail.com>
2026-08-08 08:41:15 +0700

    Viet ham nhapMang xuatMang nguyen

--- Quanlimang/Quanlimang.c
+++ Quanlimang/Quanlimang.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
 
-int main(){
+
+void nhapMang(int a[], int n);
+void xuatMang(int a[], int n);
+
+int main()
+{
+    int n = 5;
+    int mangA[n];
     
+    
+    nhapMang(mangA, n);
+    
+    printf("\n--- Mang vua nhap ---\n");
+    xuatMang(mangA, n);
+    
+    return 0;
+}
+
+void nhapMang(int a[], int n) {
+    
+    for(int i = 0; i < n; i++) {
+        printf("Nhap A[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+}
+
+void xuatMang(int a[], int n) {
+    for(int i = 0; i < n; i++) {
+        printf("A[%d] = %d\n", i, a[i]);
+    }
 }
